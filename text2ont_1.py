9fd2c1a2624ee5d0dcec4c570c8debf4874f4717
owain-Biddulph <32294891+owain-Biddulph@users.noreply.github.com>
2017-12-19 09:12:12 +0100

    Adding first text2ontology code

---
+++ text2ont_1.py
@@ -0,0 +1,83 @@
+text_list = []
+stripped_text = []
+
+key_words = []
+
+aggregation_dict = dict()
+aggregation_dict[aggregation_keys] = aggregation_values
+
+metric_dict = dict()
+metric_dict[metric_keys] = metric_values 
+
+dimension_dict = dict()
+dimension_dict[dimension_keys] = dimension_values
+
+filter_dict = dict()
+filter_dict[filter_keys] = filter_values
+
+
+		f
+	for branch in metric_dict[key] :
+		return branch_search[branch]
+
+
+
+for token in text_list:
+	if token in key_words:
+		stripped_text.append(token)
+
+
+for el in stripped_text :
+	if aggregation_dict.has_key(el) :
+		aggregation = aggregation_dict[el]
+	    
+	    	
+for el in stripped_text :
+	if metric_dict.has_key(el) :
+		for possible_metric in metric_dict[key]
+			token_count = 0
+			for token in possible_metric :
+				if token in stripped_text :
+					token_count +=1
+				if token_count = len(possible_metric) :
+					metric = possible_metric
+
+for el in stripped_text :
+	if dimension_dict.has_key(el) :
+		for possible_dimension in dimension_dict[key]
+			token_count = 0
+			for token in possible_dimension :
+				if token in stripped_text :
+					token_count +=1
+				if token_count = len(possible_dimension) :
+					dimension = possible_dimension
+
+for el in stripped_text :
+	if filter_dict.has_key(el) :
+		for possible_filter in filter_dict[key]
+			token_count = 0
+			for token in possible_filter :
+				if token in stripped_text :
+					token_count +=1
+				if token_count = len(possible_filter) :
+					filter = possible_filter
+
+
+
+# aggregation_list = []
+# dimension_list = []
+# filter_list = []
+
+# for el in text_list :
+# 	if el in aggregation_list :
+# 		aggregation = el
+
+# for el in text_list :
+# 	if el in dimension_list :
+# 		dimension_list = el
+
+# for el in text_list :
+# 	if el in filter_list :
+# 		filter = el
+
+
