9db46f9debc32a3449c2c1188822a90fe6b20d68
owain-Biddulph <32294891+owain-Biddulph@users.noreply.github.com>
2017-12-19 09:52:34 +0100

    modified text2ont_1

--- text2ont_1.py
+++ text2ont_1.py
@@ -1,25 +1,25 @@
-text_list = []
+text_list = ['je','veux','la','somme','des','charges','patronales','en','fonction','du','montant','hors','taxe','des','factures']
 stripped_text = []
 
-key_words = []
+key_words = ['somme','moyenne','nombre','charges','patronales','d\'exploitation','sur','les','societe','hors','taxe','TTC','factures','engagements','des','montant']
 
-aggregation_dict = dict()
-aggregation_dict[aggregation_keys] = aggregation_values
+#Les dictionnaires contiennent
+aggregation_dict = {'somme' : 'somme', 'moyenne' : 'moyenne', 'nombre' : 'nombre'}
+# aggregation_dict[aggregation_keys] = aggregation_values
 
-metric_dict = dict()
-metric_dict[metric_keys] = metric_values 
+metric_dict = {'charges' : [['charges','patronales'],['charges','d\'exploitation']], 'patronales': [['charges','patronales']]}
+# metric_dict[metric_keys] = metric_values 
 
-dimension_dict = dict()
-dimension_dict[dimension_keys] = dimension_values
+dimension_dict = {'montant' : [['montant','hors','taxe','des','factures'],['montant','TTC','des','factures'],['montant','des','engagements','hors','taxe']]}
+# dimension_dict[dimension_keys] = dimension_values
 
-filter_dict = dict()
-filter_dict[filter_keys] = filter_values
+filter_dict = {}
+# filter_dict[filter_keys] = filter_values
 
-
-		f
-	for branch in metric_dict[key] :
-		return branch_search[branch]
-
+aggregation = ''
+dimension = ''
+metric = ''
+filters = ''
 
 
 for token in text_list:
@@ -28,56 +28,45 @@ for token in text_list:
 
 
 for el in stripped_text :
-	if aggregation_dict.has_key(el) :
+	if el in aggregation_dict :
 		aggregation = aggregation_dict[el]
 	    
 	    	
 for el in stripped_text :
-	if metric_dict.has_key(el) :
-		for possible_metric in metric_dict[key]
+	if el in metric_dict :
+		for possible_metric in metric_dict[el] :
 			token_count = 0
 			for token in possible_metric :
 				if token in stripped_text :
 					token_count +=1
-				if token_count = len(possible_metric) :
+				if token_count == len(possible_metric) :
 					metric = possible_metric
+					break 
 
 for el in stripped_text :
-	if dimension_dict.has_key(el) :
-		for possible_dimension in dimension_dict[key]
+	if el in dimension_dict :
+		for possible_dimension in dimension_dict[el] :
 			token_count = 0
 			for token in possible_dimension :
 				if token in stripped_text :
 					token_count +=1
-				if token_count = len(possible_dimension) :
+				if token_count == len(possible_dimension) :
 					dimension = possible_dimension
+					break
 
 for el in stripped_text :
-	if filter_dict.has_key(el) :
-		for possible_filter in filter_dict[key]
+	if el in filter_dict :
+		for possible_filter in filter_dict[el] :
 			token_count = 0
 			for token in possible_filter :
 				if token in stripped_text :
 					token_count +=1
-				if token_count = len(possible_filter) :
-					filter = possible_filter
+				if token_count == len(possible_filter) :
+					filters = possible_filter
+					break
+
+print(aggregation, metric, dimension, filters)
 
 
-
-# aggregation_list = []
-# dimension_list = []
-# filter_list = []
-
-# for el in text_list :
-# 	if el in aggregation_list :
-# 		aggregation = el
-
-# for el in text_list :
-# 	if el in dimension_list :
-# 		dimension_list = el
-
-# for el in text_list :
-# 	if el in filter_list :
-# 		filter = el
 
 
