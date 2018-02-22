559585b732afd0bb3197a9469ab77a1092467c7f
ebrote <erik.brote@gmail.com>
2018-02-22 09:52:46 +0100

    Updates to primary parsing algorithm (see fasttext-gensim.py)

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Ontolgy.py
 Creates a list containing all the possible resquests.
 Creates a routing table (dictionnary) representing the different interactions inside the ontology graph.
 Creates a synonym dictionnary for every aggregation/metric/filter/dimension.
+
+# UPDATES
+
+2017-02-22 :
+    Author :    Erik
+    WHAT :      Integrated some of Owain's initial algorithm (see fasttext.py) with Gensim
+    COMMENTS :  Please comment when writing your code so that it is easier and faster to understand.
--- fasttext-gensim.py
+++ fasttext-gensim.py
@@ -1,36 +1,58 @@
 
 import speech_recognition as sr
 from gensim.models import KeyedVectors
-import nltk
+from nltk.tokenize import word_tokenize
+import numpy as np
 
-
-#def get_speech():
-
+# TODO: Turn the following section of code into a function, with loops, etc.
+# def get_speech()
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print('Say Something!')
+    print("Say Something!")
     audio = r.listen(source)
 try:
-    print(r.recognize_google(audio, language='fr-FR')) #fr-FR
+    text = r.recognize_google(audio, language='fr-FR')
+    print(text)
 except:
+    text = "Nom de l'endroit"
+    # for testing
+    # text = None
+    print("Sorry, I didn't understand that.")
     pass
 
+# initilazation of gensim word embedding
+fr_model = KeyedVectors.load_word2vec_format('wiki.fr/wiki.fr.vec')
 
-# text = get_speech()
-# print(text)
+tolkenized_text = word_tokenize(text, language='french')
+print(tolkenized_text)
 
-fr_model = KeyedVectors.load_word2vec_format('src/wiki.fr.vec')
+# TODO: French-POS tagging
 
-# # # Getting the tokens
-# # words = []
-# # for word in fr_model.vocab:
-# #     words.append(word)
+#TODO: clearer varuiable names
+word_list = np.load('word_lists/word_list.npy')
+word_matrix = np.load('word_matrices/word_matrix.npy')
+# dimension_matrix = np.load('word_matrices/dimension_matrix.npy')
+# dimension_list_fr = np.load('word_lists/dimension_list_fr.npy')
 
-# # Pick a word
-find_similar_to = "voiture"
+sentence_vector = [0 for i in range(300)]
+filtered_words = []
 
-# Finding out similar words [default= top 10]
-for similar_word in fr_model.similar_by_word(find_similar_to):
-    print("Word: {0}, Similarity: {1:.2f}".format(
-        similar_word[0], similar_word[1]
-    ))
+#primary filtration algorithm
+for word in tolkenized_text:
+    except_1 = False
+    if word[:2] == "d'" or word[:2] == "l'":
+            word = word[2:]
+    if word in fr_model:
+        filtered_words.append(word)
+    else:
+        print("{} is not in word_matrix".format(word))
+
+print(filtered_words)
+
+#getting synonyms for words
+for word in filtered_words:
+    your_word_vector = fr_model.get_vector(word)
+    synonyms = fr_model.most_similar(positive=[your_word_vector], topn=5)
+    print(synonyms)
+
+#TODO: find most similar words to those in filtrations, dimensions, etc
