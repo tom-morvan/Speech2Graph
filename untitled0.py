b80b505c692e42ef5d2de2b3052b3c93bcef6338
Catastropoulos <32413684+Catastropoulos@users.noreply.github.com>
2018-06-01 22:11:05 +0200

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  1 09:19:59 2018
-
-@author: ayoub
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  9 17:05:29 2018
-@author: Ayoub
-"""
 import flask
 
 
@@ -25,34 +12,30 @@ def HTMLForm():
   .speech input {border: 0; width: 240px; display: inline-block; height: 30px;}
   .speech img {float: right; width: 40px }
 </style>
-
 <!-- Search Form -->
 <form id="labnol" action="text2onto" method = "post">
   <div class="speech">
     <input type="text" name="q" id="transcript" placeholder="Speak" />
-
     <img onclick="startDictation()" src="//i.imgur.com/cHidSVu.gif" />
     
-
   </div>
+  
+  <input type="checkbox" name="lv" value="1"> Niveau 1
+  <input type="checkbox" name="lv" value="2"> Niveau 2
+  <input type="checkbox" name="lv" value="3"> Niveau 3 
+
   <input type="submit" value="Submit">
   
 </form>
-
 <!-- HTML5 Speech Recognition API -->
 <script>
   function startDictation() {
-
     if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
       var recognition = new webkitSpeechRecognition();
-
       recognition.continuous = false;
       recognition.interimResults = false;
-
       recognition.lang = "fr-FR";
       recognition.start();
-
       recognition.onresult = function(e) {
         document.getElementById('transcript').value
                                  = e.results[0][0].transcript;
@@ -60,11 +43,9 @@ def HTMLForm():
         recognition.stop();
       
       };
-
       recognition.onerror = function(e) {
         recognition.stop();
       }
-
     }
   }
 </script> """
@@ -79,10 +60,17 @@ def HTMLForm():
 def formPage():
     html = "<html> <body>" + HTMLForm()
     if flask.request.method == "POST":
+      level_input = flask.request.form["lv"]
       text_input = flask.request.form["q"]
-      html += "<p> This is your input: " + text_input + "</p>"
-      text_output ="ok"
-      html += "<p> This is your text: " + text_output + "</p>"
+      if len(text_input)>0:
+          html += "<p> This is your input: " + text_input + "</p>"
+          text_output ="ok"
+          html += "<p> This is your text: " + text_output + "</p>"
+      if len(level_input)>0:
+          html += "<p> This is your test level " + level_input + "</p>"
+          text_output ="ok"
+          html += "<p> This is the results of your test: " + text_output + "</p>"
+          
     html += "</body> </html>"
     return html
 
